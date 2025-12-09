c35ddfc2aab60d7b16d45eb832559e3f1c60fbac
Michał Popiel <131244219+Kogut01@users.noreply.github.com>
2025-12-09 17:18:29 +0100

    fix: docker compose

--- Engined/main.go
+++ Engined/main.go
@@ -156,7 +156,7 @@ OUTER:
 			}
 			fmt.Printf("Consumer received a task with id: %s\n", task.Id)
 			fmt.Printf("Target url %s\n", task.Target)
-			cmd := exec.Command("go", "run", "main.go", "test", "--target", task.Target, "--tests", "https", "hsts", "serv-h-a", "--taskId", task.Id)
+			cmd := exec.Command("/engine-antiginx/App", "test", "--target", task.Target, "--tests", "https", "hsts", "serv-h-a", "--taskId", task.Id)
 			cmd.Stdout = os.Stdout
 			cmd.Stderr = os.Stderr
 			cmdErr := cmd.Run()
