dc13151185f26b10fe755680970d89df8f94f624
Mailosz0812 <159311428+Mailosz0812@users.noreply.github.com>
2026-02-02 17:06:13 +0100

    Feat/191 unit test backend reporter (#214)

---
+++ App/Reporter/backend_reporter_test.go
@@ -0,0 +1,71 @@
+package Reporter
+
+import (
+	"Engine-AntiGinx/App/Tests"
+	"net/http"
+	"net/http/httptest"
+	"testing"
+)
+
+type backendReporterTest struct {
+	name            string
+	statusCode      int
+	expectedFailure int
+	expectedCalls   int
+}
+
+func TestBackendReporter_StartListening(t *testing.T) {
+	tests := []backendReporterTest{
+		{
+			name:            "Happy path,success on first try",
+			statusCode:      http.StatusOK,
+			expectedFailure: 0,
+			expectedCalls:   2,
+		},
+		{
+			name:            "Error 500, retryable",
+			statusCode:      http.StatusInternalServerError,
+			expectedFailure: 1,
+			expectedCalls:   3,
+		},
+		{
+			name:            "Error 400, non retryable",
+			statusCode:      http.StatusBadRequest,
+			expectedFailure: 1,
+			expectedCalls:   1,
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			calls := 0
+			server := httptest.NewServer(http.HandlerFunc(func(writer http.ResponseWriter, request *http.Request) {
+				calls++
+				writer.WriteHeader(tt.statusCode)
+			}))
+			defer server.Close()
+
+			resChan := make(chan Tests.TestResult)
+
+			reporter := InitializeBackendReporter(resChan, server.URL, "test-id", "target", 0, 0)
+
+			done := reporter.StartListening()
+			resChan <- Tests.TestResult{
+				Name:        "Test scan",
+				Certainty:   0,
+				ThreatLevel: 0,
+				Metadata:    nil,
+				Description: "",
+			}
+			close(resChan)
+			failedUploads := <-done
+
+			if calls != tt.expectedCalls {
+				t.Errorf("Expected calls %d, Calls made %d", tt.expectedCalls, calls)
+			}
+
+			if failedUploads != tt.expectedFailure {
+				t.Errorf("Expected failures %d, Failures %d", tt.expectedFailure, failedUploads)
+			}
+		})
+	}
+}
